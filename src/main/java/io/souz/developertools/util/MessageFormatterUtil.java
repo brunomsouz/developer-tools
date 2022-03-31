30e660234ef314173e5d7f222be48ec62c58c4ef
Bruno M. Souza <brunomsouz@gmail.com>
2022-03-30 22:12:17 -0300

    Changed package and added private constructor

--- src/main/java/io/souz/developertools/util/MessageFormatterUtil.java
+++ src/main/java/io/souz/developertools/util/MessageFormatterUtil.java
@@ -1,9 +1,13 @@
-package io.souz.developertools.handler;
+package io.souz.developertools.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class MessageHandler {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MessageFormatterUtil {
 
     public static String getMessageForLocale(String messageKey) {
         return ResourceBundle.getBundle("messages", Locale.getDefault()).getString(messageKey);
